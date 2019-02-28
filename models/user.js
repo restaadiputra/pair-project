ffc31395ac7cf42666c74623b5c898b60ddb0c85
rizalasnawi <rizalasnawi@yahoo.co.id>
2019-02-28 21:19:51 +0700

    commit

--- models/user.js
+++ models/user.js
@@ -1,12 +1,57 @@
+const hashpassword = require('../helpers/bcrypt').hashPassword
+
 'use strict';
 module.exports = (sequelize, DataTypes) => {
+  const Op = sequelize.Op
   const User = sequelize.define('User', {
     role: DataTypes.STRING,
     name: DataTypes.STRING,
-    username: DataTypes.STRING,
+    username: {
+      type:DataTypes.STRING,
+      validate : {
+        isUnique: function(value, next) {
+          User.find({
+            where:{username: value , id :{[Op.ne] : this.id}}
+          })
+         .then(data=> {
+           if(data)  next(`Username already taken`)
+           else next()
+         })
+         .catch(err => {
+           next(err)
+         })
+        }
+      }
+    },
     password: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {});
+    email: {
+      type:DataTypes.STRING, 
+      validate : {
+        isEmail: {
+          args: true, 
+          msg:"email format is incorrect"
+        },isUnique: function(value, next) {
+          User.find({
+            where:{email: value , id :{[Op.ne] : this.id}}
+          })
+         .then(data=> {
+           if(data)  next(`Email already used`)
+           else next()
+         })
+         .catch(err => {
+           next(err)
+         })
+        }
+      }
+    } 
+  }, {
+    hooks: {
+      beforeCreate : (user) => {
+        user.password = hashpassword(user.password)
+
+      }
+    }
+  });
   User.associate = function(models) {
     User.hasMany(models.Car, {
       foreignKey: 'UserId',
